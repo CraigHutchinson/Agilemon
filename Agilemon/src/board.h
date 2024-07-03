a19ce6a219bb4d2f7601326c9def2bf610d284d4
Craig Hutchinson <craig-github@crog.uk>
2024-07-04 00:28:42 +0100

    T-Wrist board pinout

--- Agilemon/src/board.h
+++ Agilemon/src/board.h
@@ -1,39 +1,64 @@
 #pragma once
 
-#if defined(LILYGO_T5_V213)
+#if defined(LILYGO_TWRIST_V1_1)
+// PIN_MOTOR= 4
+// PIN_KEY GPIO_NUM_35
+// PWR_EN 5
+// Backlight 33
+// SRAM_CS -1
 
-// SPI
-const int DIN_PIN = 23;	//SPI MOSI pin, data input
-const int SCLK_PIN = 18;	//SPI CLK pin, clock signal input
-const int CS_PIN = 5;	//Chip selection, low active
 
-const int DC_PIN = 17;	//Data/command, low for commands, high for data
-const int RST_PIN = 16;	//Reset, low active
-const int BUSY_PIN = 4;	//Busy status output pin (means busy)
+    // SPI
+    const int SPI_MOSI = 13;	//SPI MOSI pin, data input
+    const int SPI_SCLK = 14;	//SPI CLK pin, clock signal input
+    const int EPD_CS = 15;	//Chip selection, low active
+    const int EPD_DC = 2;	//Data/command, low for commands, high for data
+    const int EPD_RESET = 17;	//Reset, low active
+    const int EPD_BUSY = 16;	//Busy status output pin (means busy)
 
-const int LED_PIN = 22; //< LED
-const bool LED_ON = LOW;
+    const int LED_PIN = 33; //< BACKLIGHT!
+    const bool LED_ON = LOW;
 
-const int BUTTON_PIN = 39;
+    const int BUTTON_PIN = 5; //PWR_EN
 
-#define HAS_BATTERY 1
-const int BATTERY_PIN = 35;
+    #define HAS_BATTERY 1
+    const int BATTERY_ADC = 34;
+
+
+#elif defined(LILYGO_T5_V213)
+
+    // SPI
+    const int SPI_MOSI = 23;	//SPI MOSI pin, data input
+    const int SPI_SCLK = 18;	//SPI CLK pin, clock signal input
+    const int EPD_CS = 5;	//Chip selection, low active
+
+    const int EPD_DC = 17;	//Data/command, low for commands, high for data
+    const int EPD_RESET = 16;	//Reset, low active
+    const int EPD_BUSY = 4;	//Busy status output pin (means busy)
+
+    const int LED_PIN = 22; //< LED
+    const bool LED_ON = LOW;
+
+    const int BUTTON_PIN = 39;
+
+    #define HAS_BATTERY 1
+    const int BATTERY_ADC = 35;
 
 #elif defined(WAVESHARE_EPAPER)
 
-// SPI
-const int DIN_PIN = 14;	//SPI MOSI pin, data input
-const int SCLK_PIN = 13;	//SPI CLK pin, clock signal input
-const int CS_PIN = 15;	//Chip selection, low active
+    // SPI
+    const int SPI_MOSI = 14;	//SPI MOSI pin, data input
+    const int SPI_SCLK = 13;	//SPI CLK pin, clock signal input
+    const int EPD_CS = 15;	//Chip selection, low active
 
-const int DC_PIN = 27;	//Data/command, low for commands, high for data
-const int RST_PIN = 26;	//Reset, low active
-const int BUSY_PIN = 25;	//Busy status output pin (means busy)
+    const int EPD_DC = 27;	//Data/command, low for commands, high for data
+    const int EPD_RESET = 26;	//Reset, low active
+    const int EPD_BUSY = 25;	//Busy status output pin (means busy)
 
-const int LED_PIN = 2; //< GPIO2 has LED
-const bool LED_ON = HIGH;
+    const int LED_PIN = 2; //< GPIO2 has LED
+    const bool LED_ON = HIGH;
 
-const int BUTTON_PIN = 0;
+    const int BUTTON_PIN = 0;
 #else
 #warning "Define board"
 #endif
